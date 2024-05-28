34505450b9cd4c7b3c8b55434407fa0a1e25a845
wilbur-z <zhengwei101@aliyun.com>
2024-05-28 11:32:14 +0800

    [ADD] A slightly more complex examples

---
+++ Go/Examples/guessing-game/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	maxNum := 100
+	secretNumber := rand.Intn(maxNum)
+	fmt.Println("The secret number is ", secretNumber)
+
+	fmt.Println("Please input your guess")
+	for {
+		//使用bufio.NewReader()把Stdin转换成一个reader变量
+		//reader上有很多方便我们操作流的方法
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			continue
+		}
+		input = strings.Trim(input, "\r\n")
+
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter an integer value")
+			continue
+		}
+		fmt.Println("You guess is", guess)
+
+		if guess > secretNumber {
+			fmt.Println("Your guess is bigger than the secret number. Please try again")
+		} else if guess < secretNumber {
+			fmt.Println("Your guess is smaller than the secret number. Please try again")
+		} else {
+			fmt.Println("Correct, you Legend!")
+			break
+		}
+		continue
+	}
+}
