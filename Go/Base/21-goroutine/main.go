48fd0dc4e33db7184b390ea32010b89b8d8f5dc6
wilbur-z <zhengwei101@aliyun.com>
2024-05-30 16:46:59 +0800

    [MDF] WaitGroup for goroutine

--- Go/Base/21-goroutine/main.go
+++ Go/Base/21-goroutine/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -17,6 +18,84 @@ func HelloGoRoutine() {
 	}
 	time.Sleep(time.Second)
 }
+
+// 通过waitgroup实现协程的阻塞
+// 首先通过Add方法，对计数器+5
+// 然后开启协程，每个协程执行完后，通过Done对计数器减少1
+// 最后Wait阻塞主协程
+// 计数器为0时，退出主协程
+func ManyGoWait() {
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func(j int) {
+			defer wg.Done()
+			hello(j)
+		}(i)
+	}
+	wg.Wait()
+}
+
+func CalSquare() {
+	src := make(chan int)
+	dest := make(chan int, 3)
+
+	go func() {
+		defer close(src)
+		for i := 0; i < 10; i++ {
+			src <- i
+		}
+	}()
+	go func() {
+		defer close(dest)
+		for i := range src {
+			dest <- i * i
+		}
+	}()
+
+	for i := range dest {
+		println(i)
+	}
+}
+
+var (
+	x    int64
+	lock sync.Mutex
+)
+
+func addWithLock() {
+	for i := 0; i < 2000; i++ {
+		lock.Lock()
+		x += 1
+		lock.Unlock()
+	}
+}
+
+func addWithoutLock() {
+	for i := 0; i < 2000; i++ {
+		x += 1
+	}
+}
+
+func Add() {
+	x = 0
+	for i := 0; i < 5; i++ {
+		go addWithoutLock()
+	}
+	time.Sleep(time.Second)
+	println("WithoutLock:", x)
+
+	x = 0
+	for i := 0; i < 5; i++ {
+		go addWithLock()
+	}
+	time.Sleep(time.Second)
+	println("WithLock:", x)
+}
+
 func main() {
-	HelloGoRoutine()
+	//HelloGoRoutine()
+	ManyGoWait()
+	//CalSquare()
+	//Add()
 }
